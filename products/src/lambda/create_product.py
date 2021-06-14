b8549254933999d13260bd1f38a960b9c846fd0d
Bhavesh Sanghvi <jainbhavesh.07@gmail.com>
2021-06-13 17:44:10 -0700

    adding implementation to add product to dynamodb

--- products/src/lambda/create_product.py
+++ products/src/lambda/create_product.py
@@ -1,2 +1,19 @@
+import boto3
+
 def handler(event,context):
-    print(event)
+    client = boto3.resource('dynamodb')
+    table = client.Table('Products')
+    response = table.put_item(
+        Item={
+            'product_id': event['product_id'],
+            'product_name': event['product_name'],
+            'product_category': event['product_category'],
+            'product_description': event['product_description'],
+            'product_cost': event['product_cost']
+        }
+    )
+
+    return {
+       'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
+       'body': 'Record ' + event['product_id'] + ' added'
+   }
