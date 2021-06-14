76b310f897caa3fa88b9d8a4cf23e083254f64dd
Krupal <kjkrupal@gmail.com>
2021-06-13 23:18:28 -0400

    boiler plate code

--- products/src/utils/shared_utils.py
+++ products/src/utils/shared_utils.py
@@ -1,2 +1,10 @@
-def send_response(status_code):
-    return {"statusCode": status_code}
+def send_response(status_code, body=None):
+    """
+    A utility function to return a REST response from a lambda function.
+    """
+    return {
+        "statusCode": status_code,
+        "isBase64Encoded": "false",
+        "headers": {"Content-Type": "application/json"},
+        "body": body,
+    }
