f30f785829dbe24b0f12445785e59b46a13860f9
Bhavesh Sanghvi <jainbhavesh.07@gmail.com>
2021-06-15 20:34:39 -0700

    added validate schema code for create product

--- products/src/lambda/create_product.py
+++ products/src/lambda/create_product.py
@@ -1,10 +1,9 @@
 import os
 import json
 
-from shared_utils import send_response
-
-from schema import ProductCreateSchema
-
+from shared_utils import send_response, validate_schema
+from schema import CREATE_PRODUCT_REQUEST_SCHEMA
+from jsonschema import validate
 
 # Always read environment variables from execution context.
 PRODUCTS_TABLE_NAME = os.environ.get("PRODUCTS_TABLE_NAME")
@@ -15,11 +14,14 @@ PRODUCTS_TABLE_NAME = os.environ.get("PRODUCTS_TABLE_NAME")
 def handler(event, context):
 
     request_body = json.loads(event["body"])
-
+    
     # Validate request body
-    product_create_schema = ProductCreateSchema()
-    product_create_schema.load(request_body)
-
+    try:
+        validate_schema(CREATE_PRODUCT_REQUEST_SCHEMA, request_body)
+    except Exception e:
+        print(e)
+        print(e.__dict__)
+        return send_response(status_code=400)
     # If not valid, raise an appropriate exception and return status 400.
 
     # Use send_response method to send response when a lambda is invoked from an API gateway.
--- products/src/utils/schema.py
+++ products/src/utils/schema.py
@@ -1,12 +1,63 @@
-from marshmallow import Schema, fields
+CREATE_PRODUCT_REQUEST_SCHEMA = {
+  "$schema": "https://json-schema.org/draft/2020-12/schema",
+  "$id": "https://ecommerce.com/create_product.schema.json",
+  "title": "Create Product",
+  "description": "Create product lambda",
+  "type": "object",
+  "properties": {
+    "productName": {
+      "description": "Name of the product",
+      "type": "string"
+    },
+    "productCategory": {
+      "description": "The Category of the product",
+      "type": "string",
+      "exclusiveMinimum": 0
+    },
+    "productDescription":{
+      "description": "Desc of the product",
+      "type": "string"
+    },
+    "productPrice":{
+      "description": "Price of the product",
+      "type": "number"
+    },
+    "productPriceCurrency": {
+      "description": "Currency of the product",
+      "type": "string"
+    },
+    "productAttributes": {
+      "description": "Attributes of the product",
+      "type": "object"
+    }
+  },
+  "required": ["productName", "productPrice", "productDescription", "productCategory", "productPriceCurrency", "productAttributes"]
+}
 
 
-class ProductCreateSchema(Schema):
-    product_category = fields.Str(required=True)
-    product_name = fields.Str(required=True)
-    product_description = fields.Str(required=True)
-    product_price = fields.Number(required=True)
-    product_price_currency = fields.Str(required=True)
-    product_attributes = fields.Dict(
-        keys=fields.Str(), values=fields.Str(), required=False
-    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+#from marshmallow import Schema, fields
+
+
+
+# class ProductCreateSchema(Schema):
+#     product_category = fields.Str(required=True)
+#     product_name = fields.Str(required=True)
+#     product_description = fields.Str(required=True)
+#     product_price = fields.Number(required=True)
+#     product_price_currency = fields.Str(required=True)
+#     product_attributes = fields.Dict(
+#         keys=fields.Str(), values=fields.Str(), required=False
+#     )
